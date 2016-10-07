db1771ca6afc23e22b6ffa6fbeec0dd98acf80d3
Felquis Gimenes <frgformenton@gmail.com>
2016-10-07 10:21:33 -0300

    Fixes

--- index.js
+++ index.js
@@ -16,17 +16,21 @@
   function onControllerChange() {
     var html = '<div style="'
     html += 'position: fixed; bottom: 1em; left: 1em;'
-    html += 'background: yellow; padding: 1em;'
-    html += '">'
-    html += 'Refresh the page to see the newest content'
+
+    if (localStorage.getItem('service-worker')) {
+      html += 'background: yellow; padding: 1em;'
+      html += '">'
+      html += 'Refresh the page to see the newest content'
+    } else {
+      html += 'background: green; padding: 1em;'
+      html += '">'
+      html += 'Offline Ready'
+
+      localStorage.setItem('service-worker', 'done')
+    }
+
     html += '</div>'
 
     document.body.insertAdjacentHTML('beforeend', html)
   }
 }())
-
-// navigator.serviceWorker.register('./service-worker.js')
-
-// navigator.serviceWorker.oncontrollerchange = function () {
-//   console.log('Refresh to see the newest content')
-// }
